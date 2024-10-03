f2b4c7676037e388a00b58374175795661e6944b
Pooya Parsa <pooya@pi0.io>
2024-10-03 18:33:27 +0200

    chore: update template

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,5 @@
+import { defineNitroConfig } from "nitropack/config"
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server"
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,3 +1,5 @@
+import { eventHandler } from "#imports"
+
 export default eventHandler((event) => {
   return "Start by editing <code>server/routes/index.ts</code>.";
 });
